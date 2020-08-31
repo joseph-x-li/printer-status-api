4737c81a96b8b4ca865ab004a840fe2d9cc788ad
joseph-x-li <13826851+joseph-x-li@users.noreply.github.com>
2020-08-31 11:59:13 -0700

    Finish scraper and printer class

---
+++ scraper.py
@@ -0,0 +1,28 @@
+import requests, bs4
+from printer import Printer
+
+url = "https://clustersweb.andrew.cmu.edu/PrinterStats/All/"
+
+content = requests.get(url).content
+
+soup = bs4.BeautifulSoup(content, features="html.parser")
+
+table = soup.findAll("table")[1].findAll("tr")
+
+col_tag = ["name", "signal", "lcd_message", "status", "tray_status", "as_of"]
+
+printers = []
+
+for row in table[2:]:
+    columns = row.find_all("td")
+    args = {}
+    for c, tag in zip(columns, col_tag):
+        if tag == "signal":
+            data = c.img["alt"]
+        else:
+            data = c.get_text()
+        args[tag] = data
+    
+    printers.append(Printer(**args))
+
+printers[0].time_diff()
